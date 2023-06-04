df706123c765af53acd1e99c7de0db3c09300d3c
root <root@FujitsuStranava>
2023-06-04 20:09:57 +0200

    finished dark

--- components/AddPostButton.vue
+++ components/AddPostButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="w-40 h-12 bg-blue-800 text-slate-200 rounded-xl flex items-center justify-center gap-3 border border-gray-900 focus:bg-blue-900">
+    <button class="w-36 h-10 dark:bg-blue-800 focus:bg-slate-700 dark:text-slate-200 bg-slate-600 text-white dark:focus:bg-blue-900 dark:focus:text-slate-100 rounded-xl flex items-center justify-center gap-3">
         <PlusIcon class="w-5 h-5" />
         <span class="font-serif text-xl">New Post</span>
     </button>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,35 @@
 <template>
-  <div class="mt-10 flex justify-center flex-col max-w-2xl w-full items-center text-slate-200">
-    <div class="flex justify-between items-center w-full mb-5">
+  <div class="mt-8 flex justify-center flex-col max-w-xl w-full items-center dark:text-slate-200 text-gray-900">
+    <div class="flex justify-between items-center w-full mb-3">
       <span class="text-2xl">
         Posts
       </span>
       <AddPostButton @click="addOpened=true" />
     </div>
-    <div v-for="post in posts" class="flex flex-col w-full gap-4 bg-gray-800 rounded-md py-4 px-5 my-8 h-fit">
+    <div v-for="post in posts" class="flex flex-col w-full gap-4 dark:bg-gray-800 bg-gray-300/50 rounded-md py-4 px-5 my-6 h-fit">
       <div class="flex justify-between">
-        <span class="block text-2xl text-slate-200 font-serif">{{ post.title }}</span>
+        <span class="block text-2xl dark:text-slate-200 font-serif">{{ post.title }}</span>
         <div class="flex gap-2">
-          <span class="text-md text-slate-400">{{ post.user.name }}</span>
+          <span class="text-md dark:text-slate-400 text-black">{{ post.user.name }}</span>
           <img :src="post.user.avatar" class="rounded w-7 h-7" >
         </div>
       </div>
-      <span class="block text-md break-all px-1">{{ post.content }}</span>
+      <span class="block text-md break-all dark:text-slate-200 text-black px-1">{{ post.content }}</span>
       <div class="flex gap-1 text-xs items-end justify-between">
-        <span class="text-slate-400 text-sm">{{ dayjs(post.createdAt).format('H:m D.M.YYYY') }}</span>
+        <span class="dark:text-slate-400 text-slate-500 text-sm">{{ dayjs(post.createdAt).format('H:m D.M.YYYY') }}</span>
         <div class="flex gap-2">
           <div class="flex items-center gap-1" @click="addLike(post)">
-            {{ post.likes }}
-            <HeartIcon :class="`h-4 w-4 ${post.liked ? 'text-red-600' : 'text-slate-100/60'}`" />
+            <span class="text-gray-800 dark:text-slate-200">{{ post.likes }}</span>
+            <HeartIcon :class="`h-4 w-4 ${post.liked ? 'dark:text-red-600 text-red-700' : 'dark:text-slate-100/60 text-gray-700'}`" />
           </div>
-          <div v-if="canDeletePost(post)" class="text-red-600 focus:text-red-400" @click="deletePost(post)">
+          <div v-if="canDeletePost(post)" class="dark:text-red-600 text-red-700" @click="deletePost(post)">
             <TrashIcon class="h-5 w-5" />
           </div>
         </div>
       </div>
     </div>
-    <div class="absolute top-5 right-5 text-slate-300" @click="changeTheme">
+    <div class="absolute top-5 right-5 dark:text-slate-300 text-gray-800" @click="changeTheme">
       <SunIcon class="w-8 h-8" />
-      <span class="text-slate-200 dark:text-blue-500">theme</span>
-    </div>
-    <div>
-
     </div>
   </div>
   <div v-if="addOpened" class="fixed top-0 backdrop-blur-md bg-slate-100/5 w-full h-full flex justify-center">
@@ -44,11 +40,11 @@
           <textarea @keydown.enter.shift.exact.prevent="addNewPost" v-model="content" type="text" class="block bg-gray-800 placeholder:text-slate-300 px-3 w-full rounded-md min-h-full h-24 text-slate-100 text-sm scrollbar-hide" placeholder="Add text or image url" />
         </div>
         <div class="flex justify-between px-2">
-          <button @click="addOpened=false, title='', content=''" class="text-xs w-w-fit justify-center text-red-500 flex items-center gap-1 py-1 px-2 font-mono rounded-lg border-red-500 border bg-gray-700 focus:bg-red-800 focus:text-slate-100 focus:border-slate-100">
+          <button @click="addOpened=false, title='', content=''" class="text-xs w-w-fit justify-center text-red-500 flex items-center gap-1 py-1 px-2 font-mono rounded-lg border-red-500 border bg-gray-700">
             <XMarkIcon class="w-6 h-6" />
             Cancel
           </button>
-          <button @click="addNewPost" :class="`${disabledAddPost ? 'opacity-50' : ''} text-sm w-fit justify-center text-green-500 flex flex items-center gap-1 py-1 px-2 font-mono rounded-lg border-green-500 border bg-gray-700 focus:bg-green-700 focus:text-slate-100 focus:border-slate-100`" :disabled="disabledAddPost">
+          <button @click="addNewPost" :class="`${disabledAddPost ? 'opacity-50' : ''} text-sm w-fit justify-center text-green-500 flex flex items-center gap-1 py-1 px-2 font-mono rounded-lg border-green-500 border bg-gray-700`" :disabled="disabledAddPost">
             <PlusIcon class="w-6 h-6" />
             <div class="flex-col text-xs">
               Add Post
